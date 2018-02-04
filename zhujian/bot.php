e2203a374e4d826aa78a9679bf11f461f271fa04
linksgo2011 <120377843@qq.com>
2018-02-04 12:21:34 +0800

    add new file

---
+++ zhujian/bot.php
@@ -0,0 +1,10 @@
+<!--header data-am-widget="header" class="am-header am-header-default am-no-layout ft-header"-->
+<div data-am-widget="navbar" style="background: url(../images/bop.jpg); box-shadow: 0px -3px 3px #555 ;" class="am-navbar am-cf am-navbar-default sq-foot am-no-layout ft-foot">
+  <ul class="am-navbar-nav am-cf am-avg-sm-4">
+    <li> <a href="index.php" class="curr"> <span class="am-icon-home" style="height:29px;"></span> <span class="am-navbar-label">首页</span> </a> </li>
+    <li> <a href="zlcx.php" class="curr"> <span class="am-icon-clipboard" style="height:29px;"></span> <span class="am-navbar-label">资料查询</span> </a> </li>
+    <li> <a href="#" class="curr"> <span class="am-icon-shopping-cart" style="height:29px;"></span> <span class="am-navbar-label">购物商城</span> </a> </li>
+    <li> <a href="#" class="curr"> <span class="am-icon-video-camera" style="height:29px;"></span> <span class="am-navbar-label">视频直播</span> </a> </li>
+    <li> <a href="moreall.php" class="curr"><span class="am-icon-ellipsis-h" style="height:29px;"></span> <span class="am-navbar-label">更多</span> </a> </li>
+  </ul>
+</div>
