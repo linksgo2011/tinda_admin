ba2bdb6949f7b3ac69293bcb54b63fa9a79faa04
nlin <nlin@thoughtworks.com>
2018-03-04 08:36:40 +0800

    change error reporting

--- include/global.php
+++ include/global.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-    error_reporting(0);
+    error_reporting(E_ERROR);
 	header("content-type:text/html; charset=utf8");
 	define('XXCMS_ROOT', str_replace("\\", '/', substr(dirname(__FILE__), 0, -7)));
 	$http_ref=isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
