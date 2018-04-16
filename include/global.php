29978ec1368f15100e6a7476b27a995270689938
nlin <nlin@thoughtworks.com>
2018-04-16 23:24:56 +0800

    add log feature

--- include/global.php
+++ include/global.php
@@ -5,11 +5,15 @@
 	define('XXCMS_ROOT', str_replace("\\", '/', substr(dirname(__FILE__), 0, -7)));
 	$http_ref=isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
 	set_include_path(XXCMS_ROOT.'lib/');
-	require_once(XXCMS_ROOT."include/config.php");
+    date_default_timezone_set('PRC');
+
+    require_once(XXCMS_ROOT."include/config.php");
 	require_once(XXCMS_ROOT."include/sql-zl.php");
 	require_once("Mysql.Class.php");
+
+    require_once(XXCMS_ROOT."include/log.php");
+
 	//ʵ�����ݿ����
 	$db=new mysql($mydbhost,$mydbuser,$mydbpw,$mydbname,$conn,$mydbcharset);
 
-	date_default_timezone_set('PRC');
 ?>
