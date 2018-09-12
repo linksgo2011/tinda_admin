0f507bbbb894c8ec367491a697c0484d7703d8f8
nlin <nlin@thoughtworks.com>
2018-09-12 20:16:30 +0800

    turn off error information

--- include/global.php
+++ include/global.php
@@ -1,8 +1,9 @@
 <?php
 	session_start();
 
-    ini_set("display_errors", "On");
-    error_reporting(E_ALL^E_NOTICE^E_WARNING^E_DEPRECATED);
+//    ini_set("display_errors", "On");
+//    error_reporting(E_ALL^E_NOTICE^E_WARNING^E_DEPRECATED);
+    error_reporting(0);
 	header("content-type:text/html; charset=utf8");
 	define('XXCMS_ROOT', str_replace("\\", '/', substr(dirname(__FILE__), 0, -7)));
 	$http_ref=isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
