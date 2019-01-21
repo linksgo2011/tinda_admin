157b237818a84ce78c18d601f9b3fcbfd4945a2d
Lin Ning <nlin@thoughtworks.com>
2019-01-21 20:08:44 +0800

    fix the vip issue

--- api/OnlineHandler.php
+++ api/OnlineHandler.php
@@ -31,7 +31,7 @@ class OnlineHandler extends BaseHandler{
             "id[=]" => $uuid
         ]);
 
-        if(strtotime($user['end_date']) >= time()){
+        if(strtotime($user['end_date']) <= time()){
             $db->update("feedbackinfo",
                 array('vip'=>0,'us_koner'=>''),
                 array("id[=]"=>$user['id'])
